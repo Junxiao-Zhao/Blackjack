29d1199b8c4d8beb39a9797d387c74a9a8d5501a
Junxiao <zhao_junxiao@hotmail.com>
2023-04-05 19:27:29 -0400

    finish

--- BlackJack.py
+++ BlackJack.py
@@ -36,8 +36,9 @@ class BlackJack:
         :return: updated total of current player after this turn, winner, and loser
         """
 
-        num_d = self.choose_dice(my_count,
-                                 your_count)  # choose the number of dices
+        probs = self.choose_dice(my_count, your_count)
+        num_d = choices(range(self.n_dice + 1),
+                        probs)[0]  # choose the number of dices
         result = sum(choices(range(1, self.n_side + 1),
                              k=num_d))  # sum of this turn
         my_player.append((my_count, your_count, num_d))  # update state
@@ -54,10 +55,30 @@ class BlackJack:
         return my_count, winner, loser
 
     def play(self) -> None:
-        """Play the game n times"""
+        """Play the game n times and write the output the file"""
 
         for _ in range(self.n_game):
             self.play_once()
+
+        correct_dices = np.zeros((self.l_target, self.l_target))
+        related_prob = np.zeros((self.l_target, self.l_target))
+
+        for x in range(self.l_target):
+            for y in range(self.l_target):
+                j = np.argmax(self.choose_dice(x, y))
+                correct_dices[x, y] = j
+                denom = self.win_count[x, y, j] + self.lose_count[x, y, j]
+                prob_win = self.win_count[x, y, j] / denom if denom > 0 else 0
+                related_prob[x, y] = prob_win
+
+        with open("prog3_output.txt", 'w') as f:
+            f.write(
+                f"Game: NDice={self.n_dice}, NSides={self.n_side}, LTarget={self.l_target}, UTarget={self.u_target}\n\n"
+            )
+            f.write("Exact solution:\n\nPLAY =\n")
+            np.savetxt(f, correct_dices, fmt="\t%d")
+            f.write('\nPROB =\n')
+            np.savetxt(f, related_prob, fmt='\t%.4f')
 
     def play_once(self) -> None:
         """Play the game once"""
@@ -84,20 +105,20 @@ class BlackJack:
         for x, y, j in loser:
             self.lose_count[x, y, j] += 1
 
-    def choose_dice(self, x: int, y: int) -> int:
+    def choose_dice(self, x: int, y: int) -> list:
         """Choose the number of dices with largest probability of winning
 
         :param x: the current point count for the player about to play
         :param y: the point count for the opponent
-        :return: the num of dices
+        :return: a list of winning probability
         """
 
-        k = len(self.lose_count[x][y]) - 1  # n_dices
-        b, f_b = 0, 0.5  # the value of j with the highest value of f_j
-        fj_list = [0]
+        k = self.n_dice  # n_dices
+        b, f_b = 0, 0  # the value of j with the highest value of f_j
+        fj_list = []
         t = 0  # the total number of games that have gone through state (x, y)
 
-        for j in range(1, k + 1):  # calculate f_j
+        for j in range(k + 1):  # calculate f_j
             denom = self.win_count[x, y, j] + self.lose_count[x, y, j]
             f_j = self.win_count[x, y, j] / denom if denom > 0 else 0.5
 
@@ -109,21 +130,21 @@ class BlackJack:
 
         g = sum(fj_list) - f_b  # sum over all j that are not b
         p_b = (t * f_b + self.m) / (t * f_b + k * self.m)
-        pj_list = [0]
+        pj_list = []
 
-        for j in range(1, len(fj_list)):  # calculate p_j
+        for j in range(len(fj_list)):  # calculate p_j
             if j != b:
                 p_j = (1 - p_b) * (t * fj_list[j] +
                                    self.m) / (g * t + (k - 1) * self.m)
                 pj_list.append(p_j)
             else:
                 pj_list.append(p_b)
-        print(pj_list)
-        return np.argmax(pj_list)  # the num max prob
+
+        return pj_list
 
 
 if __name__ == "__main__":
-    lose_count = np.array([[[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0],
+    """ lose_count = np.array([[[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0],
                             [0, 0, 0, 0]],
                            [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0],
                             [0, 0, 0, 0]],
@@ -134,12 +155,12 @@ if __name__ == "__main__":
                           [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0],
                            [0, 0, 0, 0]],
                           [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0],
-                           [0, 0, 3, 1]]])
-    """ lose_count = np.zeros((15, 15, 3))
-    win_count = np.zeros((15, 15, 3))
-    print(choose_dice(2, 3, lose_count, win_count, 4))
-    play_game(2, 6, 15, 17, lose_count, win_count, 4) """
+                           [0, 0, 3, 1]]]) """
 
-    g = BlackJack(2, 6, 15, 17, 10, 4)
-    for i in range(10):
-        g.play_once()
+    g = BlackJack(n_dice=2,
+                  n_side=2,
+                  l_target=4,
+                  u_target=5,
+                  m=100,
+                  n_game=100000)
+    g.play()
